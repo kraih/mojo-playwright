560d314453417243962e60a5ac728d4bb027b827
Sebastian Riedel <sri@cpan.org>
2021-03-25 22:06:18 +0100

    Make it more obvious

--- t/wrappers/change_title.pl
+++ t/wrappers/change_title.pl
@@ -6,14 +6,13 @@ use Mojo::File qw(curfile);
 my $daemon = Mojo::Server::Daemon->new(listen => ['http://*?fd=3']);
 $daemon->load_app(curfile->dirname->dirname->sibling('chat.pl'));
 
-#
-# Here you can customize the app (database, fixtures...)
-#
 $daemon->app->log->level('warn');
 $daemon->app->config->{titles}{dashboard} = 'Bye';
 
+#
+# Here you can customize the app (database, fixtures...)
+#
 $daemon->run;
-
 #
 # Here you can place cleanup code (delete database schema, files...)
 #
