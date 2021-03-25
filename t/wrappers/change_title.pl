115208fe4734b0bd8d9d72932d02e7a138807805
Sebastian Riedel <sri@cpan.org>
2021-03-25 22:04:55 +0100

    Add comments for where to add custom code

--- t/wrappers/change_title.pl
+++ t/wrappers/change_title.pl
@@ -5,6 +5,15 @@ use Mojo::File qw(curfile);
 
 my $daemon = Mojo::Server::Daemon->new(listen => ['http://*?fd=3']);
 $daemon->load_app(curfile->dirname->dirname->sibling('chat.pl'));
+
+#
+# Here you can customize the app (database, fixtures...)
+#
 $daemon->app->log->level('warn');
 $daemon->app->config->{titles}{dashboard} = 'Bye';
+
 $daemon->run;
+
+#
+# Here you can place cleanup code (delete database schema, files...)
+#
